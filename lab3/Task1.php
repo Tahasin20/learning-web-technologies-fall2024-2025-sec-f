2c5d4a0e09d6a97cf175f1183ecad081a025ed8c
Tahasin20 <tahasinahmedsiam@gmail.com>
2024-11-11 16:19:31 +0600

    Add files via upload

---
+++ lab3/Task1.php
@@ -0,0 +1,12 @@
+<?php
+
+    $length = 4;
+    $width = 3;
+    $area = $length+$width;
+    $perameter = 2*($length+$width);
+    echo "Length: ".$length."<br>";
+    echo "Width: ".$width."<br>";
+    echo "Area: ".$area."<br>";
+    echo "Perameter: ".$perameter;
+
+?>
---
+++ lab3/Task3.php
@@ -0,0 +1,11 @@
+<?php
+
+    $Number = 4;
+    if($Number%2 == 0){
+        echo "Even";
+    }
+    else if($Number%2 != 0){
+        echo "Odd";
+    }
+
+?>
